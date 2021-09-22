e8c369d13661c5d325bac366b2ad014b1b7c417c
zoilo95 <zoilo_gomez@hotmail.com>
2021-09-22 11:34:53 +0200

    Zoilo 22-09-2021  11:35 BaseProgramacion_Ej11 Solucion 1.

--- BaseProgramacion_Ej11/src/paquete/Ejecuta.java
+++ BaseProgramacion_Ej11/src/paquete/Ejecuta.java
@@ -1,5 +1,8 @@
 package paquete;
 
+import java.util.Calendar;
+import java.util.Scanner;
+
 public class Ejecuta {
 
 	public static void main(String[] args) {
@@ -13,7 +16,60 @@ public class Ejecuta {
 		    */
 		
 		
-
+		int anyoNacimiento;
+		String nombre;
+		String nombreMenor = "";
+		int suma = 0;
+		int edad;
+		int contEdad = 0;
+		float media;
+		int menor = Integer.MAX_VALUE;
+		boolean continuar = true;
+		Calendar calendario;
+		Scanner sc;
+		
+		sc = new Scanner(System.in);
+		calendario = Calendar.getInstance();
+		
+		int anyoActual = Calendar.getInstance().get(Calendar.YEAR);
+		
+		do {
+			
+			System.out.println("Introduzca nombre:");
+			nombre = sc.nextLine();
+			
+			if(nombre.equals("fin")) {
+				continuar = false;
+			}else {
+				System.out.println("Introduzca año de nacimiento:");
+				anyoNacimiento = Integer.valueOf(sc.nextLine());
+				
+				edad = anyoActual-anyoNacimiento;
+				contEdad++;
+				suma = suma + edad;
+				
+				if(edad<menor)  {
+					menor = edad;
+					nombreMenor = nombre;
+				}
+			}
+			
+			
+				
+			
+		}while(continuar==true);
+		
+		
+		
+		System.out.println("Fin");
+		
+		if(contEdad>0) {
+			media = (float)suma/contEdad;
+			System.out.println("El más joven se llama " + nombreMenor + " y la media es " + media);
+		}
+		
+		
+		
 	}
 
 }
